5853cae6b20f175973723845705955656b22cb05
AndreasRaisl <andreas.raisl@gmx.de>
2019-06-27 13:53:48 +0200

    bugfixes

--- js/gallery.js
+++ js/gallery.js
@@ -1,19 +1,26 @@
 
 function swapMainImage(thumbnail)
 {
-    console.log("in Swap Image Function");
+    console.log("in Swap Image Function");    
     let sourceBigImage = thumbnail.dataset.largeVersion;
     let mainImage = document.querySelector("#gallery-photo img");
+
     mainImage.setAttribute("src", sourceBigImage);
     mainImage.setAttribute("alt", thumbnail.alt);
 
+    let classNameCurrentImage = "current";
+    document.querySelector("." + classNameCurrentImage).classList.remove(classNameCurrentImage);
+    thumbnail.parentNode.classList.add(classNameCurrentImage);
+
+    document.querySelector("#gallery-info h3").innerHTML = thumbnail.dataset.title;
+    document.querySelector("#gallery-info p").innerHTML = thumbnail.dataset.description;
 }
 
 function activateGallery()
 {
     alert("Hello from the gallery js file");
 
-    let thumbnails = document.querySelector("#gallery-thumbs").querySelectorAll("img");
+    let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
 
     thumbnails.forEach(function(thumbnail) {
         thumbnail.addEventListener("click", function() {
@@ -21,4 +28,5 @@ function activateGallery()
         })
     })
 }
+
 
