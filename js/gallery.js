cbdc715666b09f25f47ade94171b628fca421626
AndreasRaisl <andreas.raisl@gmx.de>
2019-06-27 11:40:18 +0200

    some JS changes

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,23 @@
+
+function swapMainImage(thumbnail)
+{
+    console.log("in Swap Image Function");
+    let sourceBigImage = thumbnail.data-large-version;
+    let mainImage = document.querySelector("#gallery-photo img");
+    mainImage.setAttribute("src", sourceBigImage);
+
+}
+
 function activateGallery()
 {
     alert("Hello from the gallery js file");
+
+    let thumbnails = document.querySelector("#gallery-thumbs").querySelectorAll("img");
+
+    thumbnails.forEach(function(thumbnail) {
+        thumbnail.addEventListener("click", function() {
+            swapMainImage(this);
+        })
+    })
 }
 
